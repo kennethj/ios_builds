06999e3e26348252f30ab5b3c01103a9fb80e75a
Kenneth Jones <github@kennethdjones.com>
2024-02-23 14:36:00 -0600

    initial commit

---
+++ Hello/Hello/HelloApp.swift
@@ -0,0 +1,17 @@
+//
+//  HelloApp.swift
+//  Hello
+//
+//  Created by Kenneth Jones on 2/23/24.
+//
+
+import SwiftUI
+
+@main
+struct HelloApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
